2bedb0764cef0f5aa1521e3e1712ff6d28c64791
Jari Mäkelä <jari.makela@reaktor.com>
2021-01-25 10:15:40 +0200

    Document

--- dotfiles/emacs.d/init.el
+++ dotfiles/emacs.d/init.el
@@ -1,3 +1,5 @@
+;; Set nice keybindings for Finnish keyboard
+;; https://github.com/auramo/emacs-osx-finnish-keyboard-fix
 (defun aq-binding (any) nil)
 (load  "~/.emacs.d/aquamacs-tools.el")
 (load  "~/.emacs.d/emulate-mac-keyboard-mode.el")
