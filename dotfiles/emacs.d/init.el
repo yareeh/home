413ae2f3d4395a9b507412a2c2fadabc8fac9cde
Jari Mäkelä <jari.makela@reaktor.com>
2021-01-25 10:22:14 +0200

    Add boomer.el

--- dotfiles/emacs.d/init.el
+++ dotfiles/emacs.d/init.el
@@ -11,3 +11,10 @@
 (setq visible-bell t)
 (if (fboundp 'tool-bar-mode) (tool-bar-mode -1))
 (setq default-directory (concat (getenv "HOME") "/"))
+
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(default ((t (:inherit nil :stipple nil :background "White" :foreground "Black" :inverse-video nil :box nil :strike-through nil :overline nil :underline nil :slant normal :weight normal :height 180 :width normal :foundry "nil" :family "Menlo")))))
