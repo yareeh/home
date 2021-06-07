0172b520904fb5d7ae5ae7bd504da2e6f64bc5cc
Jari Mäkelä <jari.makela@reaktor.com>
2021-06-07 08:54:59 +0300

    Add rust mode

--- dotfiles/emacs.d/init.el
+++ dotfiles/emacs.d/init.el
@@ -12,9 +12,29 @@
 (if (fboundp 'tool-bar-mode) (tool-bar-mode -1))
 (setq default-directory (concat (getenv "HOME") "/"))
 
+;; Rust mode
+(require 'package)
+(add-to-list 'package-archives
+             '("melpa" . "https://melpa.org/packages/") t)
+(package-initialize)
+(package-refresh-contents)
+(require 'rust-mode)
+(add-hook 'rust-mode-hook
+          (lambda () (setq indent-tabs-mode nil)))
+(setq rust-format-on-save t)
+(add-hook 'rust-mode-hook 'cargo-minor-mode)
+
+
+
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
  '(default ((t (:inherit nil :stipple nil :background "White" :foreground "Black" :inverse-video nil :box nil :strike-through nil :overline nil :underline nil :slant normal :weight normal :height 180 :width normal :foundry "nil" :family "Menlo")))))
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(package-selected-packages '(cargo rust-mode)))
